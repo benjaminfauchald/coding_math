8282d720269f2c042f71c3bcf5d7e6be1ed2542f
Benjamin Fauchald <benjamin@connectica.no>
2018-07-19 21:18:25 +0700

    episode1

--- main.js
+++ main.js
@@ -1,17 +1,13 @@
 window.onload = function() {
-
   var canvas = document.getElementById("canvas"),
     context = canvas.getContext("2d"),
-    width = canvas.width = window.innerWidth;
+    width = canvas.width = window.innerWidth,
     height = canvas.height = window.innerHeight;
 
-  context.fillRect(0,0,widht,height)
-
-
   for(var i=0; i < 100; i +=1) {
-      context.BeginPath();
+      context.beginPath();
       context.moveTo(Math.random() * width, Math.random() * height);
       context.lineTo(Math.random() * width, Math.random() * height);
-      context.stroke;
+      context.stroke();
   }
 };
