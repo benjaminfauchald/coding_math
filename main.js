006c66e137f0d3d077fc17da34bd59adbe2cac36
Benjamin Fauchald <benjamin@connectica.no>
2018-07-19 20:49:04 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,10 @@
+window.onload = function() {
+
+  var canvas = document.getElementById("canvas"),
+    contect = canvas.getContext("2d"),
+    width = canvas.width = windiw.innerWitdh;
+    height = canvas.height = height.innerHeight;
+
+  context.fillRect(0,0,widht,height)
+
+}
