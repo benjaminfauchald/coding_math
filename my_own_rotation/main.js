bd51e3fd93afeb437d2064fcfa0303bc6b58b95b
Benjamin Fauchald <benjamin@connectica.no>
2018-07-28 17:07:33 +0700

    setting up own rotation matrix

---
+++ my_own_rotation/main.js
@@ -0,0 +1,191 @@
+window.onload = function() {
+
+    // Get canvas and context etc
+    var canvas = document.getElementById("canvas"),
+    context = canvas.getContext("2d"),
+    width = canvas.width = window.innerWidth,
+    height = canvas.height = window.innerHeight,
+    imagedata = context.createImageData(width, height);
+
+    // Create the image
+    function createImage(offset) {
+        // Loop over all of the pixels
+        for (var x = 0; x < width; x++) {
+            for (var y = 0; y < height; y++) {
+                // Get the pixel index
+                var pixelindex = (y * width + x) * 4;
+
+                // Generate a xor pattern with some random noise
+                var red = ((x + offset) % 256) ^ ((y + offset) % 256);
+                var green = ((2 * x + offset) % 256) ^ ((2 * y + offset) % 256);
+                var blue = 50 + Math.floor(Math.random() * 100);
+
+                // Rotate the colors
+                blue = (blue + offset) % 256;
+
+                // Set the pixel data
+                imagedata.data[pixelindex] = red; // Red
+                imagedata.data[pixelindex + 1] = green; // Green
+                imagedata.data[pixelindex + 2] = blue; // Blue
+                imagedata.data[pixelindex + 3] = 255; // Alpha
+            }
+        }
+    }
+    
+    function drawPixel(x,y,r,g,b) {
+        var pixelindex = (y * width + x) * 4;
+        var radius = 10;
+        // Set the pixel data
+        imagedata.data[pixelindex] = r; // Red
+        imagedata.data[pixelindex + 1] = g; // Green
+        imagedata.data[pixelindex + 2] = b; // Blue
+        imagedata.data[pixelindex + 3] = 255; // Alpha
+    }
+
+    /*
+     CX @ Origin X
+     CY @ Origin Y
+     X  @ Point X to be rotated
+     Y  @ Point Y to be rotated
+    */
+    function rotate(X, Y, CX, CY, angle) {
+        var rad = angle * Math.PI / 180.0;
+        var nx = Math.cos(rad) * (X-CX) - Math.sin(rad) * (Y-CY) + CX;
+        var ny = Math.sin(rad) * (X-CX) + Math.cos(rad) * (Y-CY) + CY;
+
+        return [nx, ny];
+        //return [100, 100];
+    }
+
+// Create the image
+
+var points = [
+    {x:000,y:000,z:000},
+    {x:100,y:000,z:000},
+    {x:000,y:100,z:000}
+];
+
+var origin = [{
+        x: 000,
+        y: 000,
+        z: 000
+    }
+];
+
+
+
+function rotate2d(x,y,angle){
+//    x = x * cos(angle) - y * sin(angle)
+//    y = x * sin(angle) + y * cos(angle)
+    nx = (x * Math.cos(angle)) - (y * Math.sin(angle));
+    ny = (y * Math.sin(angle)) + (y * Math.cos(angle));
+    return[nx,ny]
+};
+
+// Draw the image data to the canvas
+context.putImageData(imagedata, 0, 0);
+
+
+function drawPoints(points) {
+    var radius = 10;
+    points.forEach(p => {
+
+        x = (width / 2) + p.x; //tranlsate points to screen cords
+        y = (height / 2) + p.y; //tranlsate points to screen cords
+
+
+//        p.x = p.x + (width / 2) + (p.x / 2);
+//        p.y = p.y + (height / 2) + (p.y / 2);
+//        drawPixel(p.x, p.y, 0, 0, 0);
+
+//        angle = performance.now() / 1000 / 6 * 2 * Math.PI;
+
+        rotated_points = rotate2d(x, y, 00);
+
+        x = rotated_points[0];
+        y = rotated_points[1];
+//        p.x = p.x + (width / 2) + (p.x / 2);
+//        p.y = p.y + (height / 2) + (p.y / 2);
+
+        console.log("x: " + x);
+        console.log("y: " + y);
+
+        context.beginPath();
+        context.arc(x, y, radius, 0, 2 * Math.PI, false);
+        context.fillStyle = 'green';
+        context.fill();
+        context.lineWidth = 5;
+        context.strokeStyle = '#003300';
+        context.stroke();
+
+
+    });
+};
+
+//------------------------------------------------------------------------------
+
+drawPoints(points);
+drawPoints(points);
+        
+
+    // Main loop
+    function main(tframe) {
+        // Request animation frames
+        window.requestAnimationFrame(main);
+
+      
+        // Create the image
+        
+
+
+        // Draw the image data to the canvas
+        //context.putImageData(imagedata, 0, 0);
+
+        // Draw the image data to the canvas
+        //context.putImageData(imagedata, 0, 0);
+
+
+    }
+
+    // Call the main loop
+    main(0);
+
+//------------------------------------------------------------------------------
+
+};
+
+// context.beginPath();
+// context.moveTo(cur_x,cur_y);
+
+// context.lineTo(cur_x + tri_width, cur_y + tri_height);
+// context.lineTo(cur_x - (tri_width), cur_y + tri_height);
+// context.lineTo(cur_x , cur_y);
+// context.stroke();
+
+// var xy = [0,0];
+
+// xy = rotate(cur_x, cur_y, cur_x, cur_y, 10);
+
+//console.log(rotate(cur_x, cur_y, cur_x, cur_y, 10));
+//console.log(rotate(cur_x, cur_y, cur_x, cur_y, 15));
+
+// cur_x =xy[0];
+// cur_y =xy[1];
+
+
+
+// context.beginPath();
+// context.moveTo(cur_x,cur_y);
+
+// context.lineTo(cur_x + tri_width, cur_y + tri_height);
+// context.lineTo(cur_x - (tri_width), cur_y + tri_height);
+// context.lineTo(cur_x , cur_y);
+// context.stroke();
+
+  //
+  // for(var i=0; i < 100; i +=1) {
+  //     context.beginPath();
+  //     context.moveTo(Math.random() * width, Math.random() * height);
+  //     context.lineTo(Math.random() * width, Math.random() * height);
+  //     context.stroke();
+  // }
